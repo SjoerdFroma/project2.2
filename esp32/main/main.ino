d63705888c2628a5494505f4d5a69e808fb841e0
Sammdorreboom <s.dorreboom@st.hanze.nl>
2023-02-02 14:54:55 +0100

    main aangepast met udp code en stifness in procenten meegenomen in het verzenden

--- esp32/main/main.ino
+++ esp32/main/main.ino
@@ -1,150 +1,91 @@
 #include <WiFi.h>
-#include <WebServer.h>
-#include <ArduinoJson.h>
+#include <WiFiUdp.h>
+#include <cJSON.h>
 #include "secret.h"
-#include <Wire.h>
- 
+
+
 #include <Adafruit_BME280.h>
 #include <Adafruit_Sensor.h>
 
-//#define PIN 4
- 
-// Web server running on port 80
-WebServer server(80);
- 
-// Sensor
+
+#define NODE_RED_IP "192.168.137.1"
+#define NODE_RED_PORT 49152
+
 Adafruit_BME280 bme;
 
-
- 
-// JSON data buffer
-StaticJsonDocument<250> jsonDocument;
-char buffer[250];
- 
 // env variable
 float temperature;
 float humidity;
 float pressure;
- 
-void connectToWiFi() {
-  Serial.print("Connecting to ");
-  Serial.println(SSID);
-  
+float stifness;
+float analogValue;
+
+
+float floatMap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+
+void WiFi_connect(void* parameter){
   WiFi.begin(SSID, PWD);
-  
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-    // we can even make the ESP32 to sleep
+      delay(500);
+      printf(".");
   }
- 
-  Serial.print("Connected. IP: ");
-  Serial.println(WiFi.localIP());
+  printf(" CONNECTED\r\n");
+  vTaskDelete(nullptr);
 
-}
- 
-void create_json(char *tag, float value, char *unit) { 
-  jsonDocument.clear(); 
-  jsonDocument["type"] = tag;
-  jsonDocument["value"] = value;
-  jsonDocument["unit"] = unit;
-  serializeJson(jsonDocument, buffer);
-  Serial.println("Buffer:");
-  Serial.println(buffer);  
-}
- 
-void add_json_object(char *tag, float value, char *unit) {
-  JsonObject obj = jsonDocument.createNestedObject();
-  obj["type"] = tag;
-  obj["value"] = value;
-  obj["unit"] = unit; 
-}
-
-void read_sensor_data(void * parameter) {
-   for (;;) {
-     temperature = bme.readTemperature();
-     humidity = bme.readHumidity();
-     pressure = bme.readPressure() / 100;
-     Serial.println("Read sensor data");
-     Serial.println(temperature);
- 
-     // delay the task
-     vTaskDelay(6000 / portTICK_PERIOD_MS);
-
-   }
-}
- 
-void getTemperature() {
-  Serial.println("Get temperature");
-  create_json("temperature", temperature, "°C");
-  server.send(200, "application/json", buffer);
-}
- 
-void getHumidity() {
-  Serial.println("Get humidity");
-  create_json("humidity", humidity, "%");
-  server.send(200, "application/json", buffer);
-}
- 
-void getPressure() {
-  Serial.println("Get pressure");
-  create_json("pressure", pressure, "mBar");
-  server.send(200, "application/json", buffer);
-}
- 
-void getEnv() {
-  Serial.println("Get env");
-  jsonDocument.clear();
-  add_json_object("temperature", temperature, "°C");
-  add_json_object("humidity", humidity, "%");
-  add_json_object("pressure", pressure, "mBar");
-  serializeJson(jsonDocument, buffer);
-  server.send(200, "application/json", buffer);
 }
 
 
-// setup API resources
-void setup_routing() {
-  server.on("/temperature", getTemperature);
-  server.on("/pressure", getPressure);
-  server.on("/humidity", getHumidity);
-  server.on("/env", getEnv);
+void udp_json_task(void *pvParameters)
+{
+    WiFiUDP udp;
+    udp.begin(NODE_RED_PORT);
 
- 
-  // start server
-  server.begin();
+
+    while (1) {
+
+
+      temperature = bme.readTemperature();
+      humidity = bme.readHumidity();
+      pressure = bme.readPressure() / 100;
+      int node = 1;
+      analogValue = analogRead(32);
+
+      stifness = floatMap(analogValue, 0, 4095, 0, 100);
+      cJSON *root = cJSON_CreateObject();
+      cJSON_AddNumberToObject(root, "node", node);
+      cJSON_AddNumberToObject(root, "temp", temperature);
+      cJSON_AddNumberToObject(root, "hum", humidity);
+      cJSON_AddNumberToObject(root, "pres", pressure);
+      cJSON_AddNumberToObject(root, "stretch", stifness);
+      char *json_string = cJSON_PrintUnformatted(root);
+
+      udp.beginPacket(NODE_RED_IP, NODE_RED_PORT);
+      udp.write((const uint8_t *)json_string, strlen(json_string));
+      udp.endPacket();
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+      cJSON_Delete(root);
+    }
+
+    vTaskDelete(NULL);
 }
 
-
-void setup_task() {
-  xTaskCreate(
-    read_sensor_data,    
-    "Read sensor data",   // Name of the task (for debugging)
-    4096,            // Stack size (bytes)
-    NULL,            // Parameter to pass
-    1,               // Task priority
-    NULL             // Task handle
-  );
-}
- 
 void setup() {
-   bme.begin(0x76);
-   Serial.begin(9600);
- 
-   // Sensor setup
-  if (!bme.begin(0x76)) {
-    Serial.println("Problem connecting to BME280");
+  bme.begin(0x76);
+  int app_cpu = xPortGetCoreID();
+  BaseType_t rc;
+  Serial.begin(115200);
+  delay(2000); // Allow USB to connect
+  xTaskCreatePinnedToCore(WiFi_connect, "WiFi_connect_task", 4096, NULL, 15, NULL, app_cpu);
 
-  }
-  connectToWiFi();
-  setup_task();
-  setup_routing();  
-
+  xTaskCreatePinnedToCore(udp_json_task, "udp_json_task", 4096, NULL, 5, NULL, app_cpu);
 
 }
- 
-void loop() {
-  server.handleClient();
 
- 
+void loop (){
+  vTaskDelete(nullptr);
 }
